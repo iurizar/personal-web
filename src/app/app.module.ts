1c6d2e4f11805436f36c0e8f86f45eb9da2a4e36
iurizar <iurizar@outlook.com>
2022-05-23 18:46:41 +0200

    Translation service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +16,14 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,        
+    TranslateModule.forRoot({            
+      loader: {                
+        provide: TranslateLoader,                
+        useFactory: HttpLoaderFactory,                
+        deps: [HttpClient]            
+      }        
+    }),
     PagesModule,
     ComponentsModule
   ],
@@ -20,3 +31,7 @@ import { PagesModule } from './pages/pages.module';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {    
+  return new TranslateHttpLoader(http);
+}
